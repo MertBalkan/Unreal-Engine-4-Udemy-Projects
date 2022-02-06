da5e2052c5acc83a006b2499e8f80283dda2e73f
Mert Balkan <mert__balkan@hotmail.com>
2022-02-06 21:21:37 +0300

    Codes (tut 116)

---
+++ Building_Escape/Source/Building_Escape/OpenDoor.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenDoor(const float DeltaTime);
+	void CloseDoor(const float DeltaTime);
+
+private:
+
+	float InitialYaw;
+	float CurrentYaw;
+
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.f;	
+
+	UPROPERTY(EditAnywhere)
+	float DoorOpenSpeed = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float DoorCloseSpeed = 0.8f;
+ 
+	float DoorCloseDelay = 2.0f;
+
+	float DoorLastOpened = 0.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	AActor *ActorThatOpens;
+};
