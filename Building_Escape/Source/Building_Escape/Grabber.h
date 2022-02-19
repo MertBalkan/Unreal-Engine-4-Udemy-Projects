586764c89119d992522ce9d8f4bb0b9de0792b1d
TheMOON <mert__balkan@hotmail.com>
2022-02-19 23:53:18 +0300

    Lecture 124

--- Building_Escape/Source/Building_Escape/Grabber.h
+++ Building_Escape/Source/Building_Escape/Grabber.h
@@ -16,14 +16,13 @@ class BUILDING_ESCAPE_API UGrabber : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UGrabber();
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
 	float Reach = 100.0f;
@@ -31,4 +30,8 @@ private:
 	UInputComponent* InputComponent = nullptr;
 	void Grab();
 	void Release();
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+	// Return the first actor within reach with physics body.
+	FHitResult GetFirstPhysicsBodyInReach() const;
 };
